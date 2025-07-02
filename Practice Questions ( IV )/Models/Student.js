98b3144adfafb1e81e2dd84639930f81b4962efd
Moonlight <aahnasharmatal@gmail.com>
2025-07-03 02:17:55 +0530

    Create Student.js

---
+++ Practice Questions ( IV )/Models/Student.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required']
+        },
+
+        age: {
+            type: Number,
+            required: [true, 'Age is required'],
+            min: [6, 'Age must be a greater than Six']
+        },
+
+        class: {
+            type: Number,
+            required: [true, 'Class is required'],
+        },
+
+        fee: {
+            type: Number,
+            required: [true, 'Fee is required']
+        },
+
+        address: {
+            city: {
+                type: String,
+                required: [true, 'City is required']
+            },
+            state: {
+                type: String,
+                required: [true, 'State is required']
+            }         
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
+
+    
