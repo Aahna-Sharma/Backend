7836e818ec6bcfac590476a5c90d810d1d3f929d
Moonlight <aahnasharmatal@gmail.com>
2025-07-20 22:51:03 +0530

    Delete Test/2. Problems: File Upload with Multer + Cloudinary (or local) directory

--- Test/2. Problems: File Upload with Multer + Cloudinary (or local)/uploadModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const uploadSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  uploadedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Upload", uploadSchema);
