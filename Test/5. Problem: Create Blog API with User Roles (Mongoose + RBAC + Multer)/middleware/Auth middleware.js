499ff87b5199924036a0a54ae18a3e0d0b7a7e28
Moonlight <aahnasharmatal@gmail.com>
2025-07-22 16:21:31 +0530

    Create Auth middleware.js

---
+++ Test/5. Problem: Create Blog API with User Roles (Mongoose + RBAC + Multer)/middleware/Auth middleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel.js");
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "No token provided" });
+
+    const decoded = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = { verifyToken };
